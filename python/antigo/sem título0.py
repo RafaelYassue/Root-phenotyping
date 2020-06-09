811e013bbb7216cf820b1487f848ccdd8ca7508c
Rafael Yassue <rafael.yassue@usp.br>
2020-06-08 21:24:44 -0300

    update

---
+++ python/antigo/sem título0.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 21:04:43 2020
+
+@author: rafae
+"""
+
+
+import cv2
+import numpy as np
+import imutils
+import matplotlib.pyplot as plt
+
+imA=cv2.imread("G:\\My Drive\\doutorado\\projeto\\dados\\imagens\\exp01\\raizes\\100NIKON\\certo\\E1_B_1")
+imA = imA[110:3000, 450:3025] #Cropping image 
+lower_green = np.array([50,50,50])
+upper_green = np.array([255,255,255])
+hsv_imgA = cv2.cvtColor(imA, cv2.COLOR_BGR2HSV)
+image = cv2.inRange(hsv_imgA, lower_green, upper_green)
+image = np.invert(image)
+
+thresh = cv2.threshold(image, 45, 255, cv2.THRESH_BINARY)[1]
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+c = max(cnts, key=cv2.contourArea)
+extLeft = tuple(c[c[:, :, 0].argmin()][0])
+extRight = tuple(c[c[:, :, 0].argmax()][0])
+extTop = tuple(c[c[:, :, 1].argmin()][0])
+extBot = tuple(c[c[:, :, 1].argmax()][0])
+cv2.drawContours(image, [c], -1, (0, 255, 255), 2)
+cv2.circle(image, extLeft, 50, (100, 10, 55), -1)
+cv2.circle(image, extRight, 50, (100, 10, 55), -1)
+cv2.circle(image, extTop, 50, (100, 10, 55), -1)
+cv2.circle(image, extBot, 50, (100, 10, 55), -1)
+
+plt.imshow(image)
+
+print(extTop)
+cv2.imshow('image', image)
+cv2.waitKey()
